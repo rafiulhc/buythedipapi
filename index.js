92f13096166357689817fc9d53bcb3065fbc9cec
rafiul hasan chowdhury <rhasan346@gmail.com>
2023-06-25 21:39:52 +0600

    bot order API created

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const { MongoClient } = require("mongodb");
+require('dotenv').config()
+app.use(express.json());
+
+app.post('/orders', async (req, res) => {
+    let { orderData } = req.body;
+    const newOrder = { orderData };
+    try {
+        const client = await MongoClient.connect(
+            `mongodb+srv://${process.env.MONGODB_ORDERS_USER}:${process.env.MONGODB_ORDERS_PASSWORD}@bedrock.ydco1gh.mongodb.net/`
+          );
+
+          const db = client.db("bedrock");
+          const collection = db.collection(process.env.MONGODB_COLLECTION_ORDERS);
+
+
+          await collection.insertOne(newOrder, (err, result) => {
+            client.close();
+            if (err) {
+              return res.status(500).json({ message: "Orders Error from API", err });
+            }
+            return res.status(200).json({ message: "Orders Added to MongoDB database collection", result });
+          });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'An error occurred' });
+    }
+  });
+
+  // Start the server
+  const port = 3001;
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
